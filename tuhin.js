9117ea60d95f683c7d542672d676f8484997bad3
omar-faruk02 <74614887+omar-faruk02@users.noreply.github.com>
2021-02-01 16:15:35 +0600

    first commit

---
+++ tuhin.js
@@ -0,0 +1,106 @@
+/*var fruits=[12,13,7,3,5];
+ for(var i=0; i<fruits.length;i++)
+ {
+     var x=fruits[2*function ourMess(name,age,class)
+{
+    var data=name + age + class; 
+    return data;
+
+}
+
+
+
+var roll=7;
+switch(roll)
+{
+    case 1:text="you are first boy";
+    break;
+
+    case 2: text="you are the secound boy";
+    break;
+
+    case 3: text="you are the third boy";
+    break;
+
+    case 4: text="you are the fourth boy";
+    break;
+
+    case 5: text="you are the fifth boy";
+    break;
+    
+    case 6: text="you are the sixth boy";
+    break;
+
+    case 7: text="you are the seventh boy";
+    break;
+
+    default: text="you are fail";
+
+}
+console.log(text);
+
+
+var input=ourMess("tuhin",25,16);
+console.log(input);i+1]+'good';
+     console.log(x);
+ }
+*/
+
+
+ function ourMess(name,age,depertment)
+{
+    console.log('name:',name)
+    console.log('age:',age)
+    depertment()
+
+}
+ourMess(' rakib', 22, banglDepertment)
+ourMess(' tuhin', 25,  cseDepertment)
+
+function banglDepertment(){
+    console.log('he study in Bngla depertment')
+}
+
+function cseDepertment(){
+    console.log('he study in cse depertment')
+}
+
+//arguments
+function addNumber(num1,num2){
+    var sum=0;
+    for (let index = 0; index < arguments.length; index++) {
+        var x=arguments[index];
+      sum=sum+x;
+      
+        
+    }
+    return sum;
+    
+}
+var peramiter=addNumber(22,44,22,3,33,55)
+
+console.log(peramiter)
+
+
+const randomNumber=Math.random()*10000
+  var floorf=Math.floor( randomNumber)
+   console.log(floorf)
+    
+
+
+//event listener
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
