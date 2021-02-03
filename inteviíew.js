b043a85630e43a1b54fafda51954042eb14d0541
omar-faruk02 <74614887+omar-faruk02@users.noreply.github.com>
2021-02-03 19:17:26 +0600

    --- Auto-Git Commit ---

--- inteviíew.js
+++ inteviíew.js
@@ -1,14 +1,13 @@
 const students=[
-    {name:'tuhin',id:21},
-    {name:'tuhin1',id:22},
-    {name:'tuhin2',id:23},
-]
-const name1=[]
-for(let i=0;i<=students.length;i++)
-{
-    const element=students[i]
-  
-    name1.push(  element.name)
+    { name:'tuhin', id:21},
+    { name:'tuhin1', id:22},
+    { name:'tuhin2', id:23}
+   {name:'tuhin22122', id:213}
+   {name:'tuhin22124', id:213}
 
-    
-}
+];
+const names=students.map(s=>s.name)
+console.log(names)
+
+
+
