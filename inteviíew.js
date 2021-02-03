128add02f083e7aab7c74fb769166ce9437a6e01
omar-faruk02 <74614887+omar-faruk02@users.noreply.github.com>
2021-02-03 16:03:17 +0600

    --- Auto-Git Commit ---

---
+++ inteviíew.js
@@ -0,0 +1,14 @@
+const students=[
+    {name:'tuhin',id:21},
+    {name:'tuhin1',id:22},
+    {name:'tuhin2',id:23},
+]
+const name1=[]
+for(let i=0;i<=students.length;i++)
+{
+    const element=students[i]
+  
+    name1.push(  element.name)
+
+    
+}
